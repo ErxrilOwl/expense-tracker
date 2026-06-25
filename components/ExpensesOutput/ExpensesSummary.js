544c635b9613e80ef5574beecc919abfa2a960bc
Nikko <atuan.nikko@gmail.com>
2026-06-25 21:25:21 +0800

    feat: added key components for expense

---
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native"
+
+export const ExpensesSummary = () => {
+    return (
+        <View>
+            <Text>PERIOD</Text>
+            <Text>$ 177.95</Text>
+        </View>
+    )
+}
