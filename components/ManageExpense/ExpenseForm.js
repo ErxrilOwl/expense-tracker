538cdfdf7f82bdd67ad63b2763fe10f9e887cf29
Nikko <atuan.nikko@gmail.com>
2026-07-29 22:09:52 +0800

    feat: form input config

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,7 +1,27 @@
 import { View } from "react-native"
+import { Input } from "./Input"
 
 export const ExpenseForm = () => {
- return <View>
-    
- </View>   
+    const amountChangedHandler = () => {
+
+    }
+
+    return (
+        <View>
+            <Input label="Amount" textInputConfig={{
+                keyboardType: 'descimal-pad',
+                onChangeText: amountChangedHandler
+            }} />
+            <Input label="Date" textInputConfig={{
+                placeholder: 'YYYY-MM-DD',
+                maxLength: 10,
+                onChangeText: () => {}
+            }} />
+            <Input label="Description"  textInputConfig={{
+                multiline: true,
+                // autoCapitalize: 'characters',
+                // autoCorrect: false
+            }}/>
+        </View>
+    )   
 }
