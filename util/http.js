851d2fd1bae6cd1f0a66d2107d5fed0469e54347
Nikko <atuan.nikko@gmail.com>
2026-08-07 21:41:09 +0800

    feat: added get requests

--- util/http.js
+++ util/http.js
@@ -7,3 +7,21 @@ export function storeExpense(expenseData) {
         expenseData
     })
 }
+
+export async function fetchExpenses() {
+    const response = await axios.get(`${BASE_URL}/expenses.json`);
+
+    const expenses = [];
+
+    for (const key in response.data) {
+        const expenseObj = {
+            id: key,
+            amount: response.data[key].amount,
+            date: new Date(response.data[key].date),
+            description: response.data[key].description
+        }
+        expenses.push(expenseObj);
+    }
+
+    return expenses;
+}
