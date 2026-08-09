a26abe2dfd356ff1f312cddb3db39a475350fbce
Nikko <atuan.nikko@gmail.com>
2026-08-09 21:35:57 +0800

    feat: used updated and delete

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -5,7 +5,7 @@ import { GlobalStyles } from '../constants/styles';
 import { Button } from '../components/UI/Button';
 import { ExpensesContext } from '../store/expenses-context';
 import { ExpenseForm } from '../components/ManageExpense/ExpenseForm';
-import { storeExpense } from '../util/http';
+import { deleteExpense, storeExpense, updateExpense } from '../util/http';
 
 const ManageExpense = ({ route, navigation }) => {
   const expensesCtx = useContext(ExpensesContext);
@@ -23,7 +23,8 @@ const ManageExpense = ({ route, navigation }) => {
     });
   }, [navigation, isEditing]);
 
-  const deleteExpenseHandler = () => {
+  const deleteExpenseHandler = async () => {
+    await deleteExpense(editedExpenseId)
     expensesCtx.deleteExpense(editedExpenseId);
     navigation.goBack();
   };
@@ -35,6 +36,7 @@ const ManageExpense = ({ route, navigation }) => {
   const confirmHandler = async (expenseData) => {
     if (isEditing) {
       expensesCtx.updateExpense(editedExpenseId, expenseData);
+      updateExpense(editedExpenseId, expenseData);
     } else {
       const id = await storeExpense(expenseData);
       expensesCtx.addExpense({...expenseData, id: id});
